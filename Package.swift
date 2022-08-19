54929fa69763c933ff8aeeb5a0fd1bf3253c9648
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-08-19 13:51:58 +0200

    update package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,39 @@ import PackageDescription
 
 let package = Package(
   name: "Lottie",
-  platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
-  targets: [.target(name: "Lottie", path: "Sources")])
+  platforms: [
+    .iOS("11.0"),
+    .macOS("10.10"),
+    .tvOS("11.0")
+  ],
+  products: [
+    .library(
+        name: "Lottie",
+        targets: ["Lottie"]
+    )
+  ],
+  targets: [
+    .target(
+        name: "Lottie",
+        path: "Sources",
+        cSettings: [
+            .define(
+                "SWIFT_OPTIMIZATION_LEVEL",
+                to: "-O"
+            ),
+            .define(
+                "SWIFT_COMPILATION_MODE",
+                to: "wholemodule"
+            )
+        ]
+    )
+  ]
+)
+
+//:configuration = Debug
+//:completeSettings = none
+
+
+//:configuration = Debug
+//SWIFT_OPTIMIZATION_LEVEL = -O
+//SWIFT_COMPILATION_MODE = wholemodule
